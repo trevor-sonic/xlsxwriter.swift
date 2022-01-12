bee281592ecbb0673f2077fa8c766be0106894c3
Trevor <trevor.d@beydag.com>
2022-01-12 12:54:55 +0000

    target path is modified.

--- Sources/Cxlsxwriter/xlsxwriter.h
+++ Sources/Cxlsxwriter/xlsxwriter.h
@@ -1,7 +1,8 @@
 #ifdef _WIN32
 #include "C:/vcpkg/installed/x64-windows/include/xlsxwriter.h"
 #elif __APPLE__
-#include "/opt/homebrew/include/xlsxwriter.h"
+//#include "/opt/homebrew/include/xlsxwriter.h"
+#include "/usr/local/include/xlsxwriter.h"
 #elif __linux__
 #include "/usr/local/include/xlsxwriter.h"
 #endif
